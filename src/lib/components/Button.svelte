963d129283f6a2813a387b6144432a1a864775a3
Dmitriy Llama <109825093+dmitriyllama@users.noreply.github.com>
2025-07-09 23:52:08 +0300

    Added large button option

--- src/lib/components/Button.svelte
+++ src/lib/components/Button.svelte
@@ -2,9 +2,10 @@
     export let color = "black";
     export let background_color = "white";
     export let shadow_color = "#666";
+    export let large = false;
 </script>
 
-<button on:click style="color: {color}; background-color: {background_color}; box-shadow: 0 3px {shadow_color}">
+<button class:large on:click style="color: {color}; background-color: {background_color}; box-shadow: 0 3px {shadow_color}">
     <slot />
 </button>
 
@@ -12,6 +13,7 @@
     button {
         display: inline-block;
         text-decoration: none;
+        font-weight: 500;
         border: none;
         border-radius: 0.8em;
         padding: 0.8em 0.5em;
@@ -20,5 +22,11 @@
 
     button:hover {
         filter: brightness(110%);
+    }
+
+    .large {
+        font-size: 1.1rem;
+        padding-left: 1.4em;
+        padding-right: 1.4em;
     }
 </style>
