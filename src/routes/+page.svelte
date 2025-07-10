dc98933ef42fb5f53c883d1294cd6956be86fb7d
Dmitriy Llama <109825093+dmitriyllama@users.noreply.github.com>
2025-07-10 12:38:02 +0300

    Static stuff

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,52 @@
-<script>
-	import ContentBox from '$lib/components/ContentBox.svelte';
-</script>
+<svelte:head>
+	<title>Hide+Seek Notebook</title>
+	<meta name="description" content="A guide for playing Hide and Seek in a few cities" />
+</svelte:head>
 
-<ContentBox>
-	<p>Hello, world!</p>
-</ContentBox>
+<div class="legend">
+	<h1>Hide+Seek Notebook</h1>
+	<p>A guide for playing Hide and Seek in a few cities</p>
+	<a href="/rulesets">Look</a>
+</div>
+
+<style>
+	.legend {
+		display: flex;
+		flex-flow: column;
+		justify-content: center;
+		align-items: center;
+		text-align: center;
+		margin: 40px 0 20px;
+		padding: 0 10px;
+	}
+
+	h1 {
+		font-size: max(4.4vw, 2rem);
+		text-shadow: 1px 3px 1px #ddd;
+	}
+
+	p {
+		font-size: 1.1rem;
+	}
+
+	a {
+		font-size: 1.2rem;
+		font-weight: 700;
+		color: white;
+		background-color: #a11;
+		border-radius: 8px;
+		display: block;
+		padding: 20px 30px;
+		margin-top: 20px;
+		margin-bottom: 6vw;
+		box-shadow: 2px 4px 1px #611;
+	}
+
+	a:hover {
+		filter: brightness(110%);
+	}
+
+	a:active {
+		filter: brightness(85%);
+	}
+</style>
--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -1,2 +1,48 @@
-<script lang="ts">
+<script>
+	import ContentBox from '$lib/components/ContentBox.svelte';
 </script>
+
+<svelte:head>
+	<title>About — Hide+Seek Notebook</title>
+	<meta name="description" content="About the app" />
+</svelte:head>
+
+<ContentBox>
+	<h1>About</h1>
+	<p>
+		This is an app for Hide and Seek players that know, or want to learn, <b
+			>how to play the game across a large city area</b
+		>.
+	</p>
+	<p>
+		We're inspired by the Jet Lag: The Game show, their games across Switzerland and Japan, and
+		their card game presentation in New York City. City-wide Hide and Seek is <b>very fun</b>, but
+		too much of organizing the game is up to the players. Here's a utility to write about the
+		organization process, in your place!
+	</p>
+	<p>
+		Add rules, set game periods, include challenges, help interested tourists - make the game easier
+		for your friends!
+	</p>
+</ContentBox>
+
+<style>
+	h1 {
+		margin-left: 4%;
+		margin-bottom: 25px;
+	}
+
+	p {
+		font-size: 1rem;
+	}
+
+	@media screen and (min-width: 601px) {
+		h1 {
+			font-size: 2.5rem;
+		}
+
+		p {
+			font-size: 1.1rem;
+		}
+	}
+</style>
